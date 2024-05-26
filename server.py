fd107be41ef8b39e613ce50755a1ba5d0950002d
demchebakov <118482330+demchebakov@users.noreply.github.com>
2024-05-26 17:53:24 +0300

    Password hashing, auth update

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
 import os
+import hashlib
 
 app = Flask(__name__)
 
-# Перевірка наявності файлів та їх створення при відсутності
+# Функція для хешування паролів за допомогою SHA-256
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# Функція перевірки наявності необхідних файлів та їх створення при відсутності
 def check_files():
-    if not os.path.isfile('compromised_passwords.txt'):
-        with open('compromised_passwords.txt', 'w') as file:
-            file.write("password1\npassword2\npassword3\n")  # Заповнення за замовчуванням
+    # Створення файлу з хешованими паролями, якщо він не існує
+    if not os.path.isfile('compromised_passwords_hashed.txt'):
+        with open('compromised_passwords_hashed.txt', 'w', encoding='utf-8') as file:
+            file.write(hash_password("password1") + "\n")
+            file.write(hash_password("password2") + "\n")
+            file.write(hash_password("password3") + "\n")
 
 check_files()
 
-# Маршрут для аутентифікації
+# Маршрут для аутентифікації користувача
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
     data = request.json
@@ -24,31 +32,32 @@ def authenticate():
     else:
         return jsonify({"authenticated": False})
 
-# Маршрут для перевірки пароля
+# Маршрут для перевірки пароля на скомпрометованість
 @app.route('/check_password', methods=['POST'])
 def check_password():
     data = request.json
     password = data.get('password')
+    hashed_password = hash_password(password)
     compromised_passwords = set()
 
     # Завантаження скомпрометованих паролів з файлу
-    with open('compromised_passwords.txt', 'r') as file:
+    with open('compromised_passwords_hashed.txt', 'r', encoding='utf-8') as file:
         for line in file:
             compromised_passwords.add(line.strip())
 
     # Перевірка, чи є введений пароль серед скомпрометованих паролів
-    if password in compromised_passwords:
+    if hashed_password in compromised_passwords:
         return jsonify({"compromised": True})
     else:
         return jsonify({"compromised": False})
 
-# Маршрут для отримання скомпрометованих паролів
+# Маршрут для отримання хешованих скомпрометованих паролів
 @app.route('/compromised_passwords', methods=['GET'])
 def compromised_passwords():
     compromised_passwords = []
 
     # Завантаження скомпрометованих паролів з файлу
-    with open('compromised_passwords.txt', 'r') as file:
+    with open('compromised_passwords_hashed.txt', 'r', encoding='utf-8') as file:
         for line in file:
             compromised_passwords.append(line.strip())
 
